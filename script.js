c8dfb07a8afe17a014c816d56c130acfbca3c2a5
joaoleno2DS <joao.zuffa@escola.pr.gov.br>
2026-09-14 11:52:25 +0000

    commit concerto

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-=async function buscarclima(cidade){
+async function buscarclima(cidade){
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${CHAVE_API}&units=metric&lang=pt_br`
     const resposta = await fetch(url);
     const dados = await resposta.json()
