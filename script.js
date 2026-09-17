88ff391515cd391118f414f475ca6daf2b3ae18c
joaoleno2DS <joao.zuffa@escola.pr.gov.br>
2026-09-17 10:48:38 +0000

    commit push

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ async function buscarclima(cidade){
 const botaoBuscar = document.getElementById("btnBuscar");
 const campoCidade = document.getElementById("cidade");
 const divResultado = document.getElementById("resultado");
+const divPrevisao = document.getElementById('p  revisao')
 
 botaoBuscar.addEventListener("click", async function(){
     const cidade = campoCidade.value;
@@ -16,11 +17,24 @@ botaoBuscar.addEventListener("click", async function(){
         return;
     }
 
+async function realizarBusca(cidade) {
     const dados = await buscarclima(cidade);
 
-    if (dados.cod === "404"){   /* tem igualdedade me js é com 3 === */
+        if (dados.cod === "404"){   /* tem igualdedade me js é com 3 === */
         divResultado.innerHTML = "<p> Cidade não encontrada.</p>";
         return;
+    }
+
+    const iconeUrl = 'https://apenweathermap.org/img/wn/${dados.wather[0].icon}@2x.png'
+
+}
+
+
+    async function bsucarPrevisao(Cidade) {
+         const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${CHAVE_API}&units=metric&lang=pt_br`
+         const resposta = await fetch(url);
+         const dados = await resposta.json();
+         return dados;
     }
 
     divResultado.innerHTML = `
